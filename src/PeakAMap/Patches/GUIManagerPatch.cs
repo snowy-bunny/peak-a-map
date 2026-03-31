41f5aa4fd22da528332a748123df4699fe3272db
jen013 <114857139+jen013@users.noreply.github.com>
2026-03-31 00:14:05 -0700

    Fix overlapping biomes text in HUD during a custom run

--- src/PeakAMap/Patches/GUIManagerPatch.cs
+++ src/PeakAMap/Patches/GUIManagerPatch.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using HarmonyLib;
-using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
 using PeakAMap.Core;
-using PeakAMap.Utilities;
 
 namespace PeakAMap.Patches;
 [HarmonyPatch(typeof(GUIManager))]
@@ -13,54 +9,12 @@ internal class GUIManagerPatch
 {
     [HarmonyPatch(nameof(GUIManager.Start))]
     [HarmonyPostfix]
-    private static void CreateMapBiomesList()
+    private static void SaveLoadedMapBiomesPatch()
     {          
         if (MapHandler.Exists && !MapRotationHandler.Instance.CurrentlyLoading)
         {
-            AddBiomesListToHUD();
             SaveLoadedMapBiomes();
         }
-    }
-
-    private static void AddBiomesListToHUD()
-    {
-        if (!(UserConfig.ShowBiomesInHUD.Value))
-        {
-            Plugin.Log.LogInfo("ShowBiomesInHUD set to false. Not creating biomes text.");
-            return;
-        }
-
-        Plugin.Log.LogInfo("ShowBiomesInHUD set to true. Creating biomes text.");
-        GameObject mapBiomes = new GameObject("MapBiomes", typeof(RectTransform), typeof(TextMeshProUGUI), typeof(MapBiomes));
-
-        MapBiomes biomes = mapBiomes.GetComponent<MapBiomes>();
-        List<LocalizedText> localizedTxts = MapBiomes.GetBiomesLocalizedText(MapHandler.Instance.biomes);
-        biomes.BiomesTextIds = localizedTxts;
-
-        GameObject parent = GUIManager.instance.hudCanvas.gameObject;
-        mapBiomes.transform.SetParent(parent.transform, worldPositionStays: false);
-
-        TextMeshProUGUI refTmp = parent.GetComponentInChildren<AscentUI>().text;
-        RectTransform refRect = refTmp.gameObject.GetRectTransform();
-        RectTransform rect = mapBiomes.GetRectTransform();
-        rect.anchoredPosition = 
-            (Ascents._currentAscent == 0) ? 
-            refRect.anchoredPosition : 
-            new Vector2(refRect.anchoredPosition.x - 250, refRect.anchoredPosition.y);
-        rect.localScale = refRect.localScale;
-        rect.sizeDelta = new Vector2(refRect.sizeDelta.x * 5, refRect.sizeDelta.y);
-        rect.anchorMin = refRect.anchorMax;
-        rect.anchorMax = refRect.anchorMax;
-        rect.pivot = refRect.pivot;
-
-        TextMeshProUGUI tmp = mapBiomes.GetTMPro();
-        tmp.font = refTmp.font;
-        tmp.color = refTmp.color;
-        tmp.fontSize = refTmp.fontSize;
-        tmp.horizontalAlignment = refTmp.horizontalAlignment;
-        tmp.verticalAlignment = refTmp.verticalAlignment;
-        tmp.textWrappingMode = refTmp.textWrappingMode;
-        tmp.overflowMode = refTmp.overflowMode;
     }
 
     private static void SaveLoadedMapBiomes()
