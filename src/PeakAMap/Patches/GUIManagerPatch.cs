65ce6d738efaf0bb6da43faa496c5f0b30f3d321
jen013 <114857139+jen013@users.noreply.github.com>
2026-04-08 00:26:13 -0700

    Add in-game shortcuts for testing

--- src/PeakAMap/Patches/GUIManagerPatch.cs
+++ src/PeakAMap/Patches/GUIManagerPatch.cs
@@ -3,6 +3,10 @@ using HarmonyLib;
 using UnityEngine.SceneManagement;
 using PeakAMap.Core;
 
+// DEBUG ONLY
+//using UnityEngine.InputSystem;
+//using UnityEngine;
+
 namespace PeakAMap.Patches;
 [HarmonyPatch(typeof(GUIManager))]
 internal class GUIManagerPatch
@@ -31,4 +35,37 @@ internal class GUIManagerPatch
         MapRotationHandler.Instance.CurrMapRotation.FillBiomesInfo(index, out _);
         DataFilesHandler.WriteMapRotation(MapRotationHandler.Instance.CurrMapRotation);
     }
+
+    // DEBUG ONLY
+    //[HarmonyPatch(nameof(GUIManager.LateUpdate))]
+    //[HarmonyPrefix]
+    //private static void CustomMapsChanged()
+    //{
+    //    if (Keyboard.current.rightCtrlKey.wasPressedThisFrame)
+    //    {
+    //        GameObject go = GameObject.Find("Map/BL_Airport/Fences/Check In desk/AirportGateKiosk");
+    //        AirportCheckInKiosk kiosk = go.GetComponent<AirportCheckInKiosk>();
+    //        kiosk.Interact_CastFinished(Character.AllCharacters[0]);
+    //    }
+    //    if (Keyboard.current.rightAltKey.wasPressedThisFrame)
+    //    {
+    //        MapsBoard.Instance.Open();
+    //    }
+    //    if (Keyboard.current.minusKey.wasPressedThisFrame)
+    //    {
+    //        LocalizedText.SetLanguage(AddLanguageIndex(-1));
+    //    }
+    //    if (Keyboard.current.equalsKey.wasPressedThisFrame)
+    //    {
+    //        LocalizedText.SetLanguage(AddLanguageIndex(1));
+    //    }
+    //}
+
+    //private static int AddLanguageIndex(int i)
+    //{
+    //    int total = Enum.GetNames(typeof(LocalizedText.Language)).Length;
+    //    int current = (int)LocalizedText.CURRENT_LANGUAGE;
+    //    int next = (total + current + i) % total;
+    //    return next;
+    //}
 }
