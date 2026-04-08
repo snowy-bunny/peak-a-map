d809fd2c9a01806ff74aa265337119ac0370e0e0
jen013 <114857139+jen013@users.noreply.github.com>
2026-04-08 00:25:56 -0700

    Add methods to BiomeInfo to make variant names more consistent

--- src/PeakAMap/Core/BiomeInfo.cs
+++ src/PeakAMap/Core/BiomeInfo.cs
@@ -17,11 +17,15 @@ public class BiomeInfo
         { Biome.BiomeType.Roots, ["ROOTS"] } 
     };
 
+    public const int NumInfo = 3;
+
     public int BiomeTypeInt;
 
     public string Variant;
 
     public bool OpenTomb;
+
+    public bool HasVariant => !string.IsNullOrEmpty(Variant);
 
     public Biome.BiomeType biomeType => (Biome.BiomeType)BiomeTypeInt;
 
@@ -30,15 +34,68 @@ public class BiomeInfo
     public BiomeInfo(Biome biome, Component? variant = null, bool? openTomb = false)
     {
         BiomeTypeInt = (int)biome.biomeType;
-        Variant = variant?.name ?? "";
+        Variant = CleanVariant(variant?.name ?? "", biome.biomeType);
         OpenTomb = openTomb ?? false;
     }
 
     public BiomeInfo(int biomeTypeInt, string variant = "", bool openTomb = false)
     {
         BiomeTypeInt = biomeTypeInt;
-        Variant = variant;
+        Variant = CleanVariant(variant, (Biome.BiomeType)biomeTypeInt);
         OpenTomb = openTomb;
+    }
+
+    private static HashSet<string> s_rootsIgnoreInVariant = new HashSet<string>
+    { 
+        "Redwoods", 
+        "Redwood", 
+        "Variant" 
+    };
+
+    public static string CleanVariant(string variant, Biome.BiomeType biome)
+    {
+        if (biome == Biome.BiomeType.Roots)
+        {
+            return CleanRootsVariant(variant);
+        }
+        if (biome == Biome.BiomeType.Mesa)
+        {
+            return CleanMesaVariant(variant);
+        }
+        return variant;
+    }
+
+    private static string CleanRootsVariant(string variant)
+    {
+        string[] cleanArr = variant.Split(' ', '-');
+        string cleanSub;
+        for (int i = 1; i < cleanArr.Length; i++)
+        {
+            cleanSub = cleanArr[i];
+
+            if (cleanSub.Length > 0)
+            {
+                cleanSub = cleanSub.Substring(0, 1).ToUpper() + cleanSub.Substring(1).ToLower();
+            }
+
+            if (s_rootsIgnoreInVariant.Contains(cleanSub))
+            {
+                cleanSub = "";
+            }
+
+            cleanArr[i] = cleanSub;
+        }
+
+        return string.Join("", cleanArr);
+    }
+
+    private static string CleanMesaVariant(string variant)
+    {
+        if (variant.Equals("NoVariant"))
+        {
+            return "Default";
+        }
+        return variant;
     }
 
     public override string ToString()
